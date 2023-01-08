eecd9aec6fa164ceae39587dcd176a4084720e22
achyutjagini <achyut.jagini@gmail.com>
2023-01-08 19:39:16 +0530

    Add files via upload

--- producer.py
+++ producer.py
@@ -4,12 +4,24 @@ import json
 import pika
 from uuid import uuid4
 
+#pika is rabbitMQ client library for python
+
+# curl -X POST -d "location=hsr&destination=btm&time=7&seats=4&cost=100" localhost:54321/new_ride
+
+#The form field in the request body is a dictionary-like object that contains the keys and values 
+# of the form data in the request.
+#additional field 'ip_address' whose value is the IP address of the request sender.
+
 consumers = []
 
 app = Flask(__name__)
 
+
+#server that accepts request for ride
+
 @app.route('/new_ride', methods=['POST'])
 def new_ride():
+    
     body = json.dumps(request.form)
     task_id = 'task_' + uuid4().hex
 
@@ -18,8 +30,11 @@ def new_ride():
         pika.ConnectionParameters(host='rabbitmq'))
     
     # Create channel to publish to ride_match queue
+
     channel = connection.channel()
+
     channel.queue_declare(queue='ride_match', durable=True)
+
     channel.basic_publish(
         exchange='',
         routing_key='ride_match',
@@ -29,11 +44,14 @@ def new_ride():
             message_id=task_id
         ))
     
+    
     print(f'Received task {body} via POST, published to queue', flush=True)
 
     # Create new channel to publish to database queue
     channel = connection.channel()
+
     channel.queue_declare(queue='database', durable=True)
+    
     channel.basic_publish(
         exchange='',
         routing_key='database',
@@ -49,9 +67,9 @@ def new_ride():
 
 
 @app.route('/new_ride_matching_consumer', methods=['POST'])
+
 def new_ride_matching_consumer():
     consumers.append({**request.form, 'ip_address': request.remote_addr})
-    
     print(f'List of consumers: {consumers}', flush=True)
 
     return ''
--- ride_matching_consumer.py
+++ ride_matching_consumer.py
@@ -1,3 +1,4 @@
+
 # Code for Ride Matching Consumer
 import json
 import os
@@ -8,34 +9,99 @@ import time
 CONSUMER_ID = os.environ['CONSUMER_ID']
 PRODUCER_ADDRESS = os.environ['PRODUCER_ADDRESS']
 
+#The consumer then sends a POST request to the /new_ride_matching_consumer endpoint of the producer server, 
+# passing a consumer_id parameter with the value of CONSUMER_ID.
+
+
+#The time.sleep(10) function is called to make the consumer wait
+#  for 10 seconds before sending the POST request to the producer.
+
+
 time.sleep(10)
+
+#The requests.post function sends a POST request to the
+#  specified URL
+#  and returns a Response object. The URL is constructed by
+#  concatenating the PRODUCER_ADDRESS
+# variable and the endpoint path using an f-string 
+# (f'http://{PRODUCER_ADDRESS}/new_ride_matching_consumer').
+
+
+#The data parameter is used to specify the request body. 
+# In this case,
+# it is set to a string in the 'consumer_id=value' format, 
+# where value is the value of the CONSUMER_ID variable
 
 response = requests.post(
     f'http://{PRODUCER_ADDRESS}/new_ride_matching_consumer',
     data=f'consumer_id={CONSUMER_ID}')
 
+#The assert statement is a debugging tool that allows you to check 
+# if a certain condition is true. If the condition is true,
+#  the assert statement does nothing and the program continues
+#  to execute.
+
+
 assert response.status_code == 200
+
 
 connection = pika.BlockingConnection(
     pika.ConnectionParameters(host='rabbitmq'))
 channel = connection.channel()
 
+
 channel.queue_declare(queue='ride_match', durable=True)
+
+
 print(f'Consumer {CONSUMER_ID} started...', flush=True)
 
 
+#Next, the consumer establishes a connection to the RabbitMQ server and declares the ride_match queue. 
+# It then defines a callback function that will be called every time a message is consumed from the queue.
+
+#The callback function takes four arguments:
+
+#ch: the channel object that the message was consumed from.
+
+#method: the method of the message, which includes the delivery_tag property that can be used to acknowledge the message.
+
+#properties: the message properties.
+
+#body: the content of the message.
+#The callback function first decodes the body of the message and loads it into a
+# 3 Python dictionary using the json.loads function. It then adds a new field '_id' 
+# to the dictionary and sets its value to the message_id property of the properties object.
+
+
 def callback(ch, method, properties, body):
+
     body = json.loads(body.decode())
     body['_id'] = properties.message_id
+
     print(f'Consumed {body} from ride_match queue', flush=True)
 
+    #simulates ride matching time
     time.sleep(int(body['time']))
+
     print(f'Consumer ID: {CONSUMER_ID}, Task ID: {properties.message_id}', flush=True)
 
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
 
+#consumer will only consume one message at a time
 channel.basic_qos(prefetch_count=1)
+
+
+#Finally, the consumer starts consuming
+#  messages from the ride_match queue
+# by calling the basic_consume method
+#of the channel object and passing the 
+# callback function as an argument.
+
+# It then enters a loop by calling the
+#start_consuming method of the channel object, which waits for messages to arrive and calls the callback 
+#function for each message.
+
 channel.basic_consume(queue='ride_match', on_message_callback=callback)
 
 channel.start_consuming()
